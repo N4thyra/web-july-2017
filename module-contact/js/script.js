1df350f0ee3f1460910d4f43736a48614f6b7e0f
Jiri Laza <N4thyra@users.noreply.github.com>
2017-07-28 17:44:31 +0200

    fixed logic

--- module-contact/js/script.js
+++ module-contact/js/script.js
@@ -1,32 +1,50 @@
 let   contactInfoDiv = $('.contact-contacts .contact-info'),
-      arrows         = $('.contact-link'),
+      leftArrow         = $('.contact-link:first-of-type'),
+      rightArrow         = $('.contact-link:last-of-type'),
       contactDetailContent = $('.contact-detail .contact-content');
 
-arrows.on('click', function() {
+
+leftArrow.on('click', function() {
   // var $next = $('.contact-info.active').removeClass('active').next('.active');
-  $('.active + .contact-info, .contact-info:eq(0)')
-      .last().addClass('active')
-      .siblings('.active').removeClass('active');
 
-      // getting data from left side and saving them into a variable
-      let img = $('.contact-info.active').find('img').attr('src');
-      let name = $('.contact-info.active').find('.contact-name').text();
-      let position = $('.contact-info.active').find('.contact-position').text();
-      let email = $('.contact-info.active').find('.contact-email').text();
-      let phone = $('.contact-info.active').find('.contact-phone').text();
+  let current = $('.contact-info.active'),
+      prev = current.prev('.contact-info');
+  console.log(prev.length, prev)
+  if(prev.length){
+      current.removeClass('active');
+      prev.addClass('active');
+  }
 
-      // changing data in contact-detail div
-      $('.contact-detail').find('img').attr('src', img);  // changing avatar
-      $('.contact-detail .contact-name').text(name); // changing name
-      $('.contact-detail .contact-position').text(position); //changing position
-      $('.contact-detail .contact-phone').text(phone); // changing cell
-      $('.contact-detail .contact-email').text(email); // changing e-mail
+  updateContactInfo();
+
 });
+
+rightArrow.on('click', function() {
+  // var $next = $('.contact-info.active').removeClass('active').next('.active');
+
+  let current = $('.contact-info.active'),
+      next = current.next('.contact-info');
+  if(next.length){
+      current.removeClass('active');
+      next.addClass('active');
+  }
+
+  updateContactInfo();
+
+});
+
 
 contactInfoDiv.on('click', function() {
   $(this).siblings('.active').removeClass('active');
   $(this).addClass('active');
 
+  updateContactInfo();
+
+});
+
+// makes all the necessary updates on the right hand side
+function updateContactInfo() {
+  // getting data from left side and saving them into a variable
   let img = $('.contact-info.active').find('img').attr('src');
   let name = $('.contact-info.active').find('.contact-name').text();
   let position = $('.contact-info.active').find('.contact-position').text();
@@ -39,4 +57,4 @@ contactInfoDiv.on('click', function() {
   $('.contact-detail .contact-position').text(position); //changing position
   $('.contact-detail .contact-phone').text(phone); // changing cell
   $('.contact-detail .contact-email').text(email); // changing e-mail
-});
+}
